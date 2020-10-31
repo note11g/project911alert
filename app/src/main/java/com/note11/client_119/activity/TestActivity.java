a25a582dbf2922b93728643b3c5294a0c69701ec
note11 <67783062+note11g@users.noreply.github.com>
2020-10-31 20:34:15 +0900

    Update TestActivity.java

--- app/src/main/java/com/note11/client_119/activity/TestActivity.java
+++ app/src/main/java/com/note11/client_119/activity/TestActivity.java
@@ -3,11 +3,18 @@ package com.note11.client_119.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.note11.client_119.R;
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
+
+import org.w3c.dom.Document;
+
+import java.util.ArrayList;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -21,6 +28,14 @@ public class TestActivity extends AppCompatActivity {
         TMapView tMapView = new TMapView(this);
 
         tMapView.setSKTMapApiKey( "l7xxaede1cbc3d2e43569b3e4d9abfb76430" );
-        linearLayoutTmap.addView( tMapView );
+
+        TMapData tmapdata = new TMapData();
+
+        TMapPoint startpoint = new TMapPoint(37.570841, 126.985302);
+        TMapPoint endpoint = new TMapPoint(37.570841, 126.985302);
+
+        tmapdata.findPathDataAll(startpoint, endpoint, doc -> Log.d("xml", doc.toString()));
+
+
     }
 }
